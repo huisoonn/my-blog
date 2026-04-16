47b685e4e5e2a7b1a6090910a28af13f5f46c650
huisoonn <huisoonn@gmail.com>
2026-04-16 15:17:56 +0900

    블로그 이름 변경

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '불안한 개미의 사팔사팔 스토리';
+export const SITE_DESCRIPTION = '작은 발걸음으로 하루하루 기록합니다.';
